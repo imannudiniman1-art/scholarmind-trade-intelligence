140b2b1f70146da11601360fd8488de3c64f5916
imannudiniman1-art <imannudiniman1@gmail.com>
2026-08-17 07:48:12 +0700

    Update test_recommendation.py

--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -1,1 +1,6 @@
-test_recommendation.py
+from src.recommendation import generate_recommendation
+
+
+def test_generate_recommendation():
+    result = generate_recommendation("Increasing", 20, 10)
+    assert result is not None
