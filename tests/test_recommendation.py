75e9aa89915defb3da67d9fb096fefac986eb57d
imannudiniman1-art <imannudiniman1@gmail.com>
2026-08-17 15:37:59 +0700

    Update test_recommendation.py

--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -2,5 +2,7 @@ from src.recommendation import generate_recommendation
 
 
 def test_generate_recommendation():
-    result = generate_recommendation("Increasing", 20, 10)
-    assert result is not None
+    assert generate_recommendation(85, 20) == "BUY"
+    assert generate_recommendation(85, 30) == "CAUTION"
+    assert generate_recommendation(75, 20) == "WATCH"
+    assert generate_recommendation(75, 30) == "AVOID"
