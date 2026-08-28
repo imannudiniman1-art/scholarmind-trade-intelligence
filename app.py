e457a1ce13b2e3de5a6c0e7b18d212b627a3d5c4
imannudiniman1-art <imannudiniman1@gmail.com>
2026-08-28 09:10:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -617,26 +617,29 @@ for item in data:
         summary_df = pd.DataFrame(
             analysis_data
         )
-
         st.dataframe(
             summary_df[
-                [
-                    "market",
-                    "demand",
-                    "risk",
-                    "price",
-                    "cost",
-                    "quantity",
-                    "margin",
-                    "profit",
-                    "recommendation"
-                ]
-            ],
-            use_container_width=True,
-            hide_index=True
-            height=220
+           [
+            "market",
+            "demand",
+            "risk",
+            "price",
+            "cost",
+            "quantity",
+            "margin",
+            "profit",
+            "recommendation"
+        ]
+    ],
+    use_container_width=True,
+    hide_index=True,
+    height=260
+)
 
-        )
+
+        
+
+        
 
 
         # ====================================================
